6c6051e2c12930011827f936aec68f01d339f24a
RuisuGit <luizgggamez@gmail.com>
2024-07-03 20:34:54 -0300

    Fix #6

--- app/update/update.bat
+++ app/update/update.bat
@@ -4,5 +4,5 @@ set "main.file=%1" && set "url=%2" && set "securecy.path.archive=%3" && set "sec
 
 curl -o sc.cmd.update %filetoupdate.link% > nul 2>&1
 del /q sc.cmd >nul && pause && rename sc.cmd.update sc.cmd
-
-sc.cmd %main.file% %url% && pause && del %0 >nul
+timeout -t 5 /nobreak >nul
+sc.cmd %main.file% %url% && pause
