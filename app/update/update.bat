3a1339e0d3b7cb302a70c900b39e3d9f72b9fe48
RuisuGit <luizgggamez@gmail.com>
2024-07-03 20:21:39 -0300

    Another Update...

--- app/update/update.bat
+++ app/update/update.bat
@@ -1,11 +1,10 @@
 @echo on && rem Script feito por SecureCyonic
 title SecureCyonic - Updating...
-set main.file=%1 && set url=%2 && set securecy.path.archive=%3 && set securecy.name=%4 && set filetoupdate.link=%5
+set "main.file=%1" && set "url=%2" && set "securecy.path.archive=%3" && set "securecy.name=%4" && set "filetoupdate.link=%5"
 (
     :update.task
     setlocal >nul 2>&1
-    curl -o "%securecy.name%" %filetoupdate.link% > nul 2>&1
-    del /q %securecy.path.archive% >nul
+    curl -o %securecy.name% %filetoupdate.link% > nul 2>&1
     move /y "%securecy.name%" "%securecy.path.archive%" > nul 2>&1
 )
 call %securecy.path.archive%sc.cmd %main.file% %url% && pause && del %0 >nul
