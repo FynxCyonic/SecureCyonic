4300ce6e3c7f3d20ad67d9d96ff62863d01bee53
RuisuGit <luizgggamez@gmail.com>
2024-07-03 20:33:50 -0300

    Fix #5

--- app/update/update.bat
+++ app/update/update.bat
@@ -2,8 +2,6 @@
 title SecureCyonic - Updating...
 set "main.file=%1" && set "url=%2" && set "securecy.path.archive=%3" && set "securecy.name=%4" && set "filetoupdate.link=%5"
 
-:update.task
-setlocal >nul 2>&1
 curl -o sc.cmd.update %filetoupdate.link% > nul 2>&1
 del /q sc.cmd >nul && pause && rename sc.cmd.update sc.cmd
 
